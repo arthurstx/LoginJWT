a1c6166315893498d30906691deffbe57231f877
ArthurG1t <arthurteixeira.git@gmail.com>
2025-08-22 08:27:30 -0300

    feat: Register ser case finalized

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,7 +1,38 @@
-import { Prisma } from '@prisma/client'
+import type { UserRepository } from '@/repositories/users-repository.js'
 import type { User } from '@prisma/client'
+import { hash } from 'bcryptjs'
 
-export interface UserRepository {
-  findByEmail(email: string): Promise<User | null>
-  create(data: Prisma.UserCreateInput): Promise<User>
+interface RegisterUseCaseRequest {
+  email: string
+  name: string
+  password: string
+}
+
+interface RegisterUserCaseResponse {
+  user: User
+}
+
+export class RegisterUserCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUserCaseResponse> {
+    const passwordHash = await hash(password, 6)
+
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new Error()
+    }
+    const user = await this.userRepository.create({
+      name,
+      email,
+      passwordHash,
+    })
+
+    return { user }
+  }
 }
