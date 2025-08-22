fd52cc8e8b05432cd8c34ac9bc62609725f95866
ArthurG1t <arthurteixeira.git@gmail.com>
2025-08-22 11:15:05 -0300

    feat: authenticate.spec finalized

---
+++ src/services/authenticate.spec.ts
@@ -0,0 +1,57 @@
+import { InMemoryUserRepository } from '@/repositories/in-memory/in-memory-register.js'
+import { AuthenticateUseCase } from './authenticate.js'
+import { expect, it, beforeEach, describe } from 'vitest'
+import { hash } from 'bcryptjs'
+
+let userRepository: InMemoryUserRepository
+let sut: AuthenticateUseCase
+
+describe('Register Use case', () => {
+  beforeEach(() => {
+    userRepository = new InMemoryUserRepository()
+    sut = new AuthenticateUseCase(userRepository)
+  })
+
+  it('should be able to authentucate', async () => {
+    await userRepository.create({
+      name: 'maryana',
+      email: 'maryana@gmail.com',
+      passwordHash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      email: 'maryana@gmail.com',
+      password: '123456',
+    })
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    await userRepository.create({
+      name: 'maryana',
+      email: 'maryana@gmail.com',
+      passwordHash: await hash('123456', 6),
+    })
+
+    expect(() =>
+      sut.execute({
+        email: 'marcos@gmail.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(Error)
+  })
+  it('should not be able to authenticate with wrong password', async () => {
+    await userRepository.create({
+      name: 'maryana',
+      email: 'maryana@gmail.com',
+      passwordHash: await hash('123456', 6),
+    })
+
+    expect(() =>
+      sut.execute({
+        email: 'maryana@gmail.com',
+        password: '123123',
+      })
+    ).rejects.toBeInstanceOf(Error)
+  })
+})
