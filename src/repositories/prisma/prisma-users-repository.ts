0f761b6e7cea691c060242e9156c176ea602ff15
ArthurG1t <arthurteixeira.git@gmail.com>
2025-08-26 15:17:25 -0300

    feat:database query are complete

---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,44 @@
+import { prisma } from '@/lib/prisma.js'
+import { Prisma, type User } from '@prisma/client'
+import type { UserRepository } from '../users-repository.js'
+
+export class PrismaUsersRepository implements UserRepository {
+  async findByName(name: string, page: number) {
+    const list = await prisma.user.findMany({
+      where: { name: name },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+    return list.length > 0 ? list : null
+  }
+
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    return user
+  }
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    return user
+  }
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data,
+    })
+
+    return user
+  }
+  async delete(id: string) {
+    const user = await prisma.user.delete({
+      where: { id },
+    })
+    return user
+  }
+}
