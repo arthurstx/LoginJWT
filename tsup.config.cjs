ba56002bd6df59924e854931391c64d03a5d47e9
ArthurG1t <arthurteixeira.git@gmail.com>
2025-10-19 23:16:41 -0300

    .

--- tsup.config.cjs
+++ tsup.config.cjs
@@ -1,9 +1,8 @@
-// tsup.config.ts
-import { defineConfig } from 'tsup'
-import path from 'node:path'
+// tsup.config.cjs (CJS)
+const path = require('node:path')
 
-export default defineConfig({
-  entry: ['src/api-vercel.ts'],
+module.exports = {
+  entry: ['src/index.ts'],
   outDir: 'dist',
   format: ['cjs'],
   platform: 'node',
@@ -13,9 +12,8 @@ export default defineConfig({
   treeshake: true,
   tsconfig: 'tsconfig.json',
   esbuildOptions(options) {
-    // 👇 aqui sim funciona em qualquer tsup recente
     options.alias = {
       '@': path.resolve(__dirname, 'src'),
     }
   },
-})
+}
